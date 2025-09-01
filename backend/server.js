7003892662b945db8f2a7e4e4845725ee5778b78
joaopauloschulzgehring-creator <joaopauloschulzgehring@gmail.com>
2025-09-01 18:20:20 -0300

    server.js

--- backend/server.js
+++ backend/server.js
@@ -10,36 +10,32 @@ const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
     origin: "*",
-    methods: ["GET", "POST"]
+    methods: ["GET","POST"]
   }
 });
 
-// store connected users
 let onlineUsers = {};
 
 io.on("connection", (socket) => {
-  console.log("a user connected");
+  console.log("A user connected");
 
-  // when a user joins, store their info
   socket.on("join", (data) => {
     onlineUsers[socket.id] = { username: data.username, avatar: data.avatar };
     io.emit("online users", Object.values(onlineUsers));
   });
 
-  // chat messages
   socket.on("chat message", (data) => {
     io.emit("chat message", data);
   });
 
-  // when a user disconnects
   socket.on("disconnect", () => {
     delete onlineUsers[socket.id];
     io.emit("online users", Object.values(onlineUsers));
-    console.log("a user disconnected");
+    console.log("A user disconnected");
   });
 });
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
